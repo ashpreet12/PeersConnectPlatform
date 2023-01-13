d3d9a7d326e52e3a8cd34668836ecd1f1c89a28e
Ashpreet Kaur <2020kuec2034@iiitkota.ac.in>
2023-01-13 15:57:07 +0530

    Add Protected Route for authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,48 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter,Routes, Route,Navigate} from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Navigation from './components/shared/Navigation/Navigation';
+import Authenticate from './pages/Authenticate/Authenticate';
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login';
+
+const isAuthenticated = true;
+
 function App() {
     return (
         <BrowserRouter>
             <Navigation />
             <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/login' element={<Login />} />
+                <Route exact path = '/' 
+                    element = {
+                        <RequireAuth redirectTo="/rooms">
+                        <Home />
+                        </RequireAuth>
+                }
+                />
+                <Route path = '/authenticate'
+                    element = {
+                        <RequireAuth redirectTo="/rooms">
+                        <Authenticate />
+                        </RequireAuth>
+                    }
+                />
+                <Route path = '/register' exact
+                    element = {<Register/>}
+                />
+                <Route path = '/login' exact
+                    element = {<Login/>}
+                />
             </Routes>
         </BrowserRouter>
     );
 }
+
+function RequireAuth({ children, redirectTo }) {
+    return isAuthenticated ? children : <Navigate to={redirectTo} />;
+}
+
+
+
 
 export default App;
