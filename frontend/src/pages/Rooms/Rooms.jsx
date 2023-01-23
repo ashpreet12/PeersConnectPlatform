ea87c50491de9230177e62c19a2fbde9c348ffe5
Lavisha Bhambri <bhambrilavisha@gmail.com>
2023-01-23 15:56:01 +0530

    rooms and searchbar added

--- frontend/src/pages/Rooms/Rooms.jsx
+++ frontend/src/pages/Rooms/Rooms.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import styles from './Rooms.module.css';
 
 const Rooms = () => {
     return (<>
-    Room Component
-    </>);
+        <div className="container">
+            <div className={styles.roomsHeader}>
+                <div className={styles.left}>
+                    <span className={styles.heading}>All voice rooms</span>
+                    <div className={styles.searchBox}>
+                        <img src="/images/search-icon.png" alt="search" />
+                        <input type="text" className={styles.searchInput} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+);
 };
 
 export default Rooms;
