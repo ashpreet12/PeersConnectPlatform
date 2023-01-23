ce78605013326e5c877b297a783f3c730297e3fc
Lavisha Bhambri <bhambrilavisha@gmail.com>
2023-01-23 15:36:34 +0530

    navbar profile photo and username added

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -20,7 +20,7 @@ const Navigation = () => {
   };
 
   const dispatch = useDispatch();
-  const { isAuth } = useSelector((state) => state.auth);
+  const { isAuth, user } = useSelector((state) => state.auth);
 
   async function logoutUser() {
     try {
@@ -37,7 +37,30 @@ const Navigation = () => {
         <img src="/images/logo.png" alt="logo" />
         <span style={logoText}>PeersConnect</span>
       </Link>
-      {isAuth && <button onClick={logoutUser}>Logout</button>}
+      {isAuth && (
+                <div className={styles.navRight}>
+                    <h3>{user?.name}</h3>
+                    <Link to="/">
+                        <img
+                            className={styles.avatar}
+                            src={
+                                user.avatar
+                                    ? user.avatar
+                                    : '/images/monkey-avatar.png'
+                            }
+                            width="40"
+                            height="40"
+                            alt="avatar"
+                        />
+                    </Link>
+                    <button
+                        className={styles.logoutButton}
+                        onClick={logoutUser}
+                    >
+                        <img src="/images/logout.png" alt="logout" />
+                    </button>
+                </div>
+            )}
     </nav>
   );
 };
