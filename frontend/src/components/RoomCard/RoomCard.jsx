2043064e379f56260fd10e79ff53fa7a9a8070d2
Annu Sharma <annusharma5711@gmail.com>
2023-01-25 00:23:20 -0800

    add room on list

--- frontend/src/components/RoomCard/RoomCard.jsx
+++ frontend/src/components/RoomCard/RoomCard.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import styles from './RoomCard.module.css';
+import { useHistory } from 'react-router-dom';
 
-const RoomCard = ({room}) => {
+const RoomCard = ({ room }) => {
+    const history = useHistory();
     return (
-        <div className={styles.card}>
+        <div
+            onClick={() => {
+                history.push(`/room/${room.id}`);
+            }}
+            className={styles.card}
+        >
             <h3 className={styles.topic}>{room.topic}</h3>
-            <div className={styles.speakers}>
+            <div
+                className={`${styles.speakers} ${
+                    room.speakers.length === 1 ? styles.singleSpeaker : ''
+                }`}
+            >
                 <div className={styles.avatars}>
                     {room.speakers.map((speaker) => (
                         <img
@@ -27,12 +38,12 @@ const RoomCard = ({room}) => {
                     ))}
                 </div>
             </div>
-
             <div className={styles.peopleCount}>
                 <span>{room.totalPeople}</span>
                 <img src="/images/user-icon.png" alt="user-icon" />
             </div>
         </div>
-    )
-}
+    );
+};
+
 export default RoomCard;
