b8de7a9756353d349765cd3ef3ae990a6c013dc3
harshita-2402 <hmangotra@gmail.com>
2023-01-28 12:50:36 +0530

    WebRTC

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,15 @@ const DbConnect = require("./database");
 const router = require("./routes");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const ACTIONS = require("./actions");
+const server = require("http").createServer(app);
+
+const io = requite("socket.io")(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
 
 app.use(cookieParser());
 const corsOption = {
@@ -23,4 +32,28 @@ app.get("/", (req, res) => {
   res.send("Hello from express Js");
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+//sockets
+const socketUserMapping = {
+
+}
+
+io.on('connection', (socket) => {
+  console.log('new connection', socket.id);
+  socket.on(ACTIONS.JOIN, ({roomId, user}) =>{
+     socketUserMapping[socket.id] = user;
+
+     const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+
+     clients.forEach(clientId =>{
+      io.to(clientId).emit(ACTIONS.ADD_PEER, {});
+     });
+
+     socket.emit(ACTIONS.ADD_PEER, {});
+
+     socket.join(roomId);
+    //  console.log(clients);
+  });
+
+});
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useWebRTC } from '../../hooks/useWebRTC';
+import {useParams} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 const Room = () => {
-    return <div>Single room</div>;
+    const{id : roomId} = useParams();
+    const user = useSelector((state) => state.auth.user)
+    const { clients, provideRef } = useWebRTC(roomId, user);
+    return <div>
+        <h1>Allconnected clients</h1>
+        {
+            clients.map(client => {
+                return ( <div key={client.id}>
+                    <audio ref={(instance) => provideRef(instance, client.id)}
+                        controls autoPlay>
+                    </audio>
+                    <h4>{client.name}</h4>
+                    </div>
+                );
+            })
+        }
+            </div>;
     
 };
 export default Room;
