bc313c37acb54c28269e964986a97c1638db483b
Annu Sharma <annusharma5711@gmail.com>
2023-01-28 04:19:01 -0800

    mute and unmute functionality

--- backend/services/room-service.js
+++ backend/services/room-service.js
@@ -17,5 +17,9 @@ class RoomService {
             .exec();
         return rooms;
     }
+    async getRoom(roomId) {
+        const room = await RoomModel.findOne({ _id: roomId });
+        return room;
+    }
 }
 module.exports = new RoomService();
